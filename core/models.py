1b4caa5673e7884387bb5d6b18ff8e8408079ff3
Alexander Davydov <nyddle@gmail.com>
2014-12-22 02:53:02 +0300

    add author link

--- core/models.py
+++ core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+#from django.contrib.auth.models import User
 
 class Question(models.Model):
     question = models.CharField(max_length=200)
@@ -7,3 +9,5 @@ class Question(models.Model):
     b = models.CharField(max_length=200)
     c = models.CharField(max_length=200)
     created = models.DateTimeField('creation date', auto_now_add=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
+
