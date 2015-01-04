8461efc2fee310f052aadcafaac6dbd68d833041
Alexander Davydov <nyddle@gmail.com>
2015-01-04 18:37:47 +0100

    check answer

--- game/forms.py
+++ game/forms.py
@@ -10,10 +10,7 @@ class QuestionForm(forms.Form):
         self._question = kwargs.pop('question')
         super(QuestionForm, self).__init__(*args, **kwargs)
         self.fields['question_id'].initial = self._question.id
-        letters = ('a', 'b', 'c', 'd')
         answers = [self._question.a, self._question.b, self._question.c, self._question.answer]
         shuffle(answers)
-        CHOICES = [(letters[i], answers[i]) for i in range(4)]
+        CHOICES = [(answers[i], answers[i]) for i in range(4)]
         self.fields['answer'] = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect())
-
-
--- game/views.py
+++ game/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.contrib.messages import get_messages
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.generic import View
@@ -19,11 +21,16 @@ class GameView(View):
     def post(self, request):
 
         question_id = request.POST.get('question_id')
+        cquestion = Question.objects.get(id=question_id)
+
         answer = request.POST.get('answer')
 
-        cquestion = Question.objects.get(id=question_id)
+        if (answer == cquestion.answer):
+            messages.info(request, 'You\'re right!')
+        else:
+            messages.info(request, 'Wrong')
 
         question = Question.objects.all().order_by('?')[0]
         form = QuestionForm(question=question)
 
-        return render_to_response('game/game.html', { 'question' : question, 'form' : form }, RequestContext(request))
+        return render_to_response('game/game.html', { 'question' : question, 'form' : form, 'messages' : get_messages(request) }, RequestContext(request))
